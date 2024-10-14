34a384f9e4c9a3b846e73402749d29a6d9ebd93a
lbbttujj <lbbttujj18@gmail.com>
2024-10-14 23:20:58 +0300

    add voices

--- index.ts
+++ index.ts
@@ -154,7 +154,7 @@ app.post('/create-image', async (req, res) => {
         res.send('https://steamuserimages-a.akamaihd.net/ugc/5103172932219996638/4FE828816AAF7B0660DD1BD7C94EBD54C68EF692/?imw=512&amp;&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=false')
     } else {
         const imageGPT = await ai.images.generate(
-            {model: 'dall-e-2', prompt: `Сгенериуй эмоцию в виде котика: ${mood}`, size: '1024x1024', quality: 'standard', n: 1}
+            {model: 'dall-e-2', prompt: `Сгенериуй эмоцию в виде арт котика: ${mood}`, size: '512x512', quality: 'standard', n: 1}
         )
         res.send(imageGPT.data[0].url)
     }
